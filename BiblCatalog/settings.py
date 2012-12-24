ae15855248ceb0753f323e042fd5b8e5290d5e5d
voleg <8Cgbhfkb8>
2012-12-24 10:32:28 +0400

    Произвёл начальную настройку (без подключения к СУБД)

--- BiblCatalog/settings.py
+++ BiblCatalog/settings.py
@@ -1,4 +1,6 @@
 # Django settings for BiblCatalog project.
+import os
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -24,11 +26,11 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 SITE_ID = 1
 
@@ -45,18 +47,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '../sitemedia/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, '../sitemedia/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -102,7 +104,7 @@ ROOT_URLCONF = 'BiblCatalog.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'BiblCatalog.wsgi.application'
 
-TEMPLATE_DIRS = ('/home/voleg/PycharmProjects/BiblCatalog/templates',)
+TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, '../templates/'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
