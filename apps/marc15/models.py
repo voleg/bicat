d597b10fa4ed4eb2ca8e878a1dba02a2ad4c65aa
voleg <ffrooty@gmail.com>
2013-01-18 16:54:11 +0400

    Практически решил проблему выборки из поля ITEM

--- apps/marc15/models.py
+++ apps/marc15/models.py
@@ -1,47 +1,57 @@
 #coding: utf-8
 from django.db import models
 from managers import ItemManager
+from parse import parse_item_to_dict, get_marc_string, get_caption, get_marc_field
+class MarcField(models.TextField):
+    pass
 
 class Tag(models.Model):
-    """
-    Таблица TAG
-    TAG-номер поля (марковкском формате)
-    SUBTAG-буква подполя
-    CAPTION-наименование поля(подполя)
-    """
-    tag = models.CharField(max_length=3, primary_key=True, db_column='TAG') # Field name made lowercase.
-    subtag = models.CharField(max_length=1, unique=True, db_column='SUBTAG') # Field name made lowercase.
-    flags = models.IntegerField(null=True, db_column='FLAGS', blank=True) # Field name made lowercase.
-    separator = models.CharField(max_length=1, db_column='SEPARATOR', blank=True) # Field name made lowercase.
-    caption = models.CharField(max_length=40, db_column='CAPTION', blank=True) # Field name made lowercase.
+    tag = models.CharField("номер поля (марковкском формате)", max_length=3, primary_key=True, db_column='TAG')
+    subtag = models.CharField("буква подполя", max_length=1, unique=True, db_column='SUBTAG')
+    flags = models.IntegerField("Флаги", null=True, db_column='FLAGS', blank=True)
+    separator = models.CharField("Разделитель", max_length=1, db_column='SEPARATOR', blank=True)
+    caption = models.CharField("Наименование", max_length=40, db_column='CAPTION', blank=True)
+
     def __unicode__(self):
         return self.tag + self.subtag + " " + self.caption
     class Meta:
         unique_together = (("tag", "subtag"),)
         db_table = u'TAG'
         abstract = True
+        verbose_name = u'Тег'
+        verbose_name_plural = u'Теги'
 
 # Table with docs items. The Item Field in the DB hard coded in MARC format see https://bitbucket.org/voleg/bicat/wiki/Поле%20ITEM
 class Doc(models.Model):
-    """
-    Таблица DOC
-    DOC_ID-идентификатор документа
-    RECTYPE-тип записи
-    BIBLEVEL-библиотечный уровень
-    ITEM-документ в макроподобном формате
-    """
-    doc_id = models.IntegerField(primary_key=True, db_column='DOC_ID') # Field name made lowercase.
-    rectype = models.CharField(max_length=1, db_column='RECTYPE', blank=True) # Field name made lowercase.
-    biblevel = models.CharField(max_length=1, db_column='BIBLEVEL', blank=True) # Field name made lowercase.
-    item = models.TextField(db_column='ITEM', blank=True) # Field name made lowercase.
-
+    doc_id = models.IntegerField("№ Документа", primary_key=True, db_column='DOC_ID')
+    rectype = models.CharField("тип записи", max_length=1, db_column='RECTYPE', blank=True)
+    biblevel = models.CharField("библиотечный уровень", max_length=1, db_column='BIBLEVEL', blank=True)
+    item = models.TextField("документ в макроподобном формате", db_column='ITEM', blank=True)
     objects = ItemManager()
+
+    def item_author(self):
+        author = get_marc_field(self.item, tag='100', subtag='a')
+        other_authors = get_marc_field(self.item, tag='700', subtag='a')
+        return ''.join([author,',',other_authors])
+
+    def item_changetimestamp(self):
+        timestamp = get_marc_field(self.item, tag='005', subtag='0')[:14]
+        return timestamp
+
+    def item_dict_full(self):
+        return "".join([str(self.doc_id),": ",get_marc_string(parse_item_to_dict(self.item))])
+
+    def item_dict(self):
+        return ''.join([self.item_dict_full()[:600], ' ...'])
 
     def __unicode__(self):
         return str(self.doc_id) + " " + self.item[:80]
+
     class Meta:
         db_table = u'DOC'
         abstract = True
+        verbose_name = u'Документ'
+        verbose_name_plural = u'Документы'
 
 class Idx100A(models.Model):
     idx_id = models.IntegerField(db_column='IDX_ID') # Field name made lowercase.
--- apps/marc15/parse.py
+++ apps/marc15/parse.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 __author__ = 'voleg'
-# sweet parser of Marc1.5 ITEM
+"""
+sweet parser of Marc1.5 ITEM
+
+"""
+
+def get_tag_name(tag, subtag):
+    """
+    тут получаем имя тега. Тег состоит из 2-х элементов tag+subtag так уж повелось исторически ...
+    """
+    # todo переписать метод получения тегов т.к. тут очень много запросов к БД
+
+    from apps.BiCat.models import Tag as cononical_tags
+    print("%s - %s" % (tag, subtag))
+    name = cononical_tags.objects.get(tag=str(tag), subtag=str(subtag)).caption
+    print(name)
+    return name
 
 def parse_item_to_dict(item):
+    """
+    function to convert string in MARC1.5 format to Dict
+    """
     byTags = '\x1e' # Разделитель тэгов
     bySubtags = '\x1f' # Разделитель подтегов
     item_fields = {}
@@ -17,11 +35,42 @@ def parse_item_to_dict(item):
             if newSubTag in SubtagsTempDict.keys():
                 for k, v  in SubtagsTempDict.items():
                     if k == newSubTag:
-                        newCaption = v + "," + newCaption
+                        newCaption = ''.join([v, ' ,', newCaption])
             SubtagsTempDict.update({newSubTag:newCaption})
 #            print("\t %s - %s " % (newSubTag,newCaption))
         item_fields.update({newTag:SubtagsTempDict})
     return item_fields
 
+def get_marc_string(dict):
+    """Geterate a solid string from input dict """
+    tag_subtag_item = ''
+    for a,b in dict.items():
+        subtag_items = ''
+        subtag_items += ''.join(['\t\t%s\t%s\n' % (key, value) for key, value in b.items()])
+        tag_subtag_item += ''.join(['%s\n%s' % (a, subtag_items)])
+    return tag_subtag_item
+
 def get_caption(item_dict, tag=None, subtag=None):
-    return item_dict.get(tag).get(subtag)
+    """
+    Here we are looking in to Dict for some values
+    expecting an Item dict
+
+    {'TAG':{'SUBTAG':'Value'}}
+
+    """
+    if tag is not None and subtag is not None:
+        return item_dict.get(str(tag)).get(str(subtag))
+    elif tag is not None and subtag is None:
+        return item_dict.get(str(tag))
+    else:
+        return get_marc_string(item_dict)
+
+def get_marc_field(item, tag=None, subtag=None):
+    """
+    An input we have an Item as a raw string in Marc-Format and a couple of tag and subtag
+    In the output we have a Value {'TAG':{'SUBTAG':'Value'}}
+    """
+    if item:
+        return get_caption(parse_item_to_dict(item), tag=tag, subtag=subtag)
+    else:
+        raise
