12513c521246a742ba01a5497033ceb75e0288a8
voleg <ffrooty@gmail.com>
2013-02-01 10:41:16 +0400

    Доработал шаблон

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Category(models.Model):
     name = models.CharField(max_length=160)
     description = models.TextField(max_length=160)
     creator = models.ForeignKey(User, related_name="creator_set")
+
+class Article(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    slug = models.SlugField(max_length=255, blank=False, null=False)
+    creator = models.ForeignKey(User, blank=False, null=False)
+    issue = models.TextField(max_length=20480, blank=True, null=True)
+    date = models.DateTimeField(default=datetime.now())
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'issue', [self.slug]
+
+    def __unicode__(self):
+        return self.name
