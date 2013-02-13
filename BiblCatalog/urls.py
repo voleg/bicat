19b2272f0be046377f7dba506d19e2f11ff30148
voleg <xvoleg@ya.ru>
2013-02-13 10:18:21 +0400

    добавил страницы ошибок и favicon

--- BiblCatalog/urls.py
+++ BiblCatalog/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^favicon\.ico$', RedirectView.as_view(url=''.join([settings.STATIC_URL, 'favicon.ico']))),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^catalog/', include('apps.marc15.urls')),
