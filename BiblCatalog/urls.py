00700ae5fe87dc4754b3f20e6b6339ae3debe86f
voleg <xvoleg@ya.ru>
2013-03-11 11:04:11 +0400

    добавил возможность создавать и выводить небольшие заметки на главной странице

--- BiblCatalog/urls.py
+++ BiblCatalog/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^catalog/', include('apps.marc15.urls')),
     url(r'^blog/', include('apps.blog.urls')),
-    url(r'', RedirectView.as_view(url='/blog/')),
+    url(r'^$', RedirectView.as_view(url='/blog/')),
 )
