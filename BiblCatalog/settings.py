7b797b61c71dd5c7976303766c694433b11ed3de
voleg <ffrooty@gmail.com>
2013-10-19 13:46:05 +0400

    Clean up in settings
    Signed-off-by: voleg <ffrooty@gmail.com>

--- BiblCatalog/settings.py
+++ BiblCatalog/settings.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 __author__ = 'voleg'
-# Django settings for BiblCatalog project.
 import os, platform
-from .local import *
+from django.core.exceptions import ImproperlyConfigured
+try:
+    from .local import *
+except ImportError:
+    raise ImproperlyConfigured('can\'t get local settings')
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 hostname = platform.node()
@@ -71,12 +74,6 @@ else:
 
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    ('Voleg', 'ffrooty@gmail.com'),
-)
-
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': default_db,
     'bikart': {
@@ -125,69 +122,32 @@ DATABASES = {
 
 DATABASE_ROUTERS = ['apps.router.BiRouter']
 
+# Тег в поле `db`/DOC.Item который не нужно разрезать по разделителю (`,` или `;`)
 DO_NOT_SPLIT_TAGS = ['254']
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Moscow'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'ru-ru'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_PATH, '../sitemedia/media/')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_PATH, '../sitemedia/static/')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = ()
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = secret_key
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -207,8 +167,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'BiblCatalog.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'BiblCatalog.wsgi.application'
 
 TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, '../templates/'),)
@@ -254,6 +212,7 @@ INSTALLED_APPS = (
     'sitemedia',
     'apps.prefs',
     #    'sorl.thumbnail'
+    'crispy_forms'
 )
 
 # A sample logging configuration. The only tangible logging
@@ -286,7 +245,6 @@ LOGGING = {
 }
 
 ### REDIS
-
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
@@ -295,7 +253,7 @@ REDIS_PORT = 6379
 #   brew install redis      # установка на OSX
 #   redis-server            # запуск демона
 
-CACHES = {
+REDIS_CACHES = {
    'default': {
        'BACKEND': 'redis_cache.RedisCache',
        'LOCATION': '127.0.0.1:6379',
@@ -307,10 +265,13 @@ CACHES = {
        },
    }
 
-# # Разкоментировать для отключения кеширования в проекте
-#
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-#     }
-# }
+DUMMY_CACHES = {
+     'default': {
+         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+     }
+}
+
+if DEBUG:
+    CACHES=DUMMY_CACHES
+else:
+    CACHES=DUMMY_CACHES
